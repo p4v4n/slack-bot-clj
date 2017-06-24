6feff9a35417c7abb43934ddb092b4d0590752ad
p4v4n <pavan.barcelona@gmail.com>
2017-06-24 13:24:02 +0530

    initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject slack-bot-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.julienxx/clj-slack "0.5.5"]]
+  :main ^:skip-aot slack-bot-clj.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
