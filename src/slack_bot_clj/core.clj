ba055910bc1b667bfbdb4384f0dcf8ea3f0b4ec7
p4v4n <pavan.barcelona@gmail.com>
2017-06-27 12:41:02 +0530

    Add bot namespace and remove default code in main-fn

--- src/slack_bot_clj/core.clj
+++ src/slack_bot_clj/core.clj
@@ -1,7 +1,6 @@
 (ns slack-bot-clj.core
-  (:gen-class))
+  (:require [slack-bot-clj.bot :as bot]))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  )
