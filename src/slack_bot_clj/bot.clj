8667f9556c9b64342d151c72e4e7f67cf0a31b9e
p4v4n <pavan.barcelona@gmail.com>
2017-06-27 14:30:25 +0530

    Add 'send' command to post messages via bot   -add send handler to post the message in the correct channel.

--- src/slack_bot_clj/bot.clj
+++ src/slack_bot_clj/bot.clj
@@ -21,18 +21,23 @@
 		                      :type "typing"
 		                      :channel channel-id}))
 
+(defn send-handler [text]
+	(let [[s send-time channel-id send-text] (str/split text #"\s+" 4)] 
+	  (Thread/sleep 10000)
+	  (rtm/send-event dispatcher {:type "message"
+		                          :channel channel-id
+		                          :text send-text})))
+
 (defn message-handler [message]
   (let [text (:text message)
 		channel-id (:channel message)]
     (println message)
     (send-typing-indicator channel-id)
     (cond
-      (str/includes? text "hello") (rtm/send-event dispatcher {:type "message"
+      (str/starts-with? text "hello") (rtm/send-event dispatcher {:type "message"
 		                                                       :channel channel-id
 		                                                       :text "hello to you sir!"})
-      (str/includes? text "?") (rtm/send-event dispatcher {:type "message"
-		                                                   :channel channel-id
-		                                                   :text "No"}))))
+      (str/starts-with? text "send") (send-handler text))))
 
 (rtm/sub-to-event events-pub :message message-handler)
 
